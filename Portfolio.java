3123f1a55e2f7f438e123f67f5d2b553154d760d
sachinmr2005 <sachin.mohankumar.ravichandran@gmail.com>
2026-06-21 09:14:17 +0530

    Create Portfolio.java

---
+++ Portfolio.java
@@ -0,0 +1,31 @@
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<Security> securities;
+
+    public Portfolio() {}
+
+    public Portfolio(Client client, List<Security> securities) {
+        this.client = client;
+        this.securities = securities;
+    }
+
+    public Long getId() { return id; }
+
+    public Client getClient() { return client; }
+    public void setClient(Client client) { this.client = client; }
+
+    public List<Security> getSecurities() { return securities; }
+    public void setSecurities(List<Security> securities) {
+        this.securities = securities;
+    }
+}
