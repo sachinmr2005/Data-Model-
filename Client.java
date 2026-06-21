720637e7764472f100ff282fcc2d4b94a9d7e5d6
sachinmr2005 <sachin.mohankumar.ravichandran@gmail.com>
2026-06-21 09:13:03 +0530

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,51 @@
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String clientName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id")
+    private FinancialAdvisor advisor;
+
+    @OneToOne(mappedBy = "client")
+    private Portfolio portfolio;
+
+    public Client() {}
+
+    public Client(String clientName, String email, String phone,
+                  FinancialAdvisor advisor, Portfolio portfolio) {
+        this.clientName = clientName;
+        this.email = email;
+        this.phone = phone;
+        this.advisor = advisor;
+        this.portfolio = portfolio;
+    }
+
+    public Long getId() { return id; }
+
+    public String getClientName() { return clientName; }
+    public void setClientName(String clientName) { this.clientName = clientName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public FinancialAdvisor getAdvisor() { return advisor; }
+    public void setAdvisor(FinancialAdvisor advisor) { this.advisor = advisor; }
+
+    public Portfolio getPortfolio() { return portfolio; }
+    public void setPortfolio(Portfolio portfolio) { this.portfolio = portfolio; }
+}
